11390e44e2a6ac2ab7cf6ee37876a8047b99e393
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2020-09-03 12:43:34 +0900

    Rename backup.gz

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import datetime
 import subprocess
 import requests
 import json
@@ -12,17 +13,19 @@ def send_discord(failed):
 
     if failed:
         discord_body = {
-            "content": "MongoDB backup failed. Check console for more details"
+            "content": "MongoDB backup failed. Check console for more details."
         }
 
     requests.post(os.environ.get("DISCORD_WEBHOOK"), json.dumps(discord_body), headers={"Content-Type":"application/json"})
 
+with open("credentials.json", mode="w") as f:
+    f.write(os.environ.get("GOOGLE_CREDENTIALS_JSON"))
 
-process = subprocess.run(["mongodump", "--uri", os.environ.get("MONGODB_URI"), "--gzip", "--archive=backup.gz"])
+process = subprocess.run(["mongodump", "--uri", os.environ.get("MONGODB_URI"), "--gzip", "--archive=mongodump-{date}.gz".format(date=datetime.datetime.now().strftime('%Y%m%d%H%M%S'))])
 if process.returncode != 0:
     send_discord(True)
     quit()
 
-# TODO: Upload backup.gz to somewhere
+# TODO: Upload backup.gz to Google Drive
 
 send_discord(False)
