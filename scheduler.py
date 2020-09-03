f57ec03167881ba3aec70dff5bdb3394fef7b8d0
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2020-09-03 11:17:42 +0900

    Send error message when backup failed

--- scheduler.py
+++ scheduler.py
@@ -4,12 +4,25 @@ import requests
 import json
 import os
 
-subprocess.run(["mongodump", "--uri", os.environ.get("MONGODB_URI"), "--gzip", "--archive=backup.gz"])
+process = subprocess.run(["mongodump", "--uri", os.environ.get("MONGODB_URI"), "--gzip", "--archive=backup.gz"])
+if process.returncode != 0:
+    send_discord(True)
+    return
+
+send_discord(False)
+    
 
 # TODO: Upload backup.gz to somewhere
+
+def send_discord(failed):
 
 discord_body = {
     "content": "MongoDB backup completed!"
 }
+
+if failed:
+    discord_body = {
+        "content": "MongoDB backup failed. Check console for more details"
+    }
 
 requests.post(os.environ.get("DISCORD_WEBHOOK"), json.dumps(discord_body), headers={"Content-Type":"application/json"})
