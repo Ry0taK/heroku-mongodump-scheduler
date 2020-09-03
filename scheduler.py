308c3376d891c3c46c21bde5720b2cc47713bbcd
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2020-09-03 10:12:30 +0900

    Working on MongoDump

--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
+import subprocess
+import requests
+import json
+import os
 
-print("Hello World!")
+print(subprocess.run(["mongodump", "--uri", os.environ.get("MONGODB_URI"), "--gzip", "--archive=backup.gz"], capture_output=True))
+
+discord_body = {
+    "content": "MongoDB backup completed!"
+}
+
+requests.post(os.environ.get("DISCORD_WEBHOOK"), json.dumps(discord_body), headers={"Content-Type":"application/json"})
